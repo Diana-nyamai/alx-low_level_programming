a97012967919e0c39338a5cfb8663f4f78f55cef
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-17 06:36:11 -0800

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int Sch = 'a';
+	int Bch = 'A';
+	while (Sch <= 'z' && Bch <= 'Z')
+	{
+		putchar(Sch);
+		putchar(Bch);
+	}
+	putchar('\n');
+	return (0);
+}
