f2391c44a2957968b52939b2672265116db636f6
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-21 06:15:21 -0800

    update

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - check main
+ * @n: input
+ * Description: function print_sign()
+ * Return: 0
+ */
+
+int print_sign(int n)
+{
+	int neg = -1;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		neg = 1;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		neg = 0;
+	}
+	else
+	{
+		_putchar('-');
+		return (neg);
+	}
+}
