ee446f329e58562dcb53ecbd10f64b4ee5419b18
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-21 10:44:40 -0800

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - check main
+ * @a @b: input
+ * Description: function add()
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int res;
+	res = a + b;
+	return (res);
+}
