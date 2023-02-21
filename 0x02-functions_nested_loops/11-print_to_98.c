4ffbe9c2d27107c53967230e95fa205c8dd5bd9d
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-21 10:59:22 -0800

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - check main
+ * @n: input
+ * Description: function print_to_98()
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 99; i++)
+	{
+		printf("%d, ", i); 
+	}
+}
