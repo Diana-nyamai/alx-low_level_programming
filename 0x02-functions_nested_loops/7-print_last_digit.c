933c2554e7d2de206792915a54b1be061812d91f
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-21 06:51:28 -0800

    update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check main
+ * @n: input
+ * Description: function print_last_digit()
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int res;
+
+	res = n % 10;
+	return res;
+}
