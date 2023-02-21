844cdb7a51001bc9f3ce30087acf410813d1f8a5
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-21 05:08:03 -0800

    update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,11 +9,12 @@
 int _islower(int c)
 {
 	char i;
+	int lower = 0;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		if (c != i)
-			return 0;
+		if (c == i)
+			lower = 1;
 	}
-	return 1;
+	return (lower);
 }
