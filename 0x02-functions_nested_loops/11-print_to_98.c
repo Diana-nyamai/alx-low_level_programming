08343aacb35239f7ef666fbf26c081d3d220af20
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-21 11:21:17 -0800

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,8 +12,26 @@ void print_to_98(int n)
 {
 	int i;
 
-	for (i = n; i < 99; i++)
+	if (n <= 98)
 	{
-		printf("%d, ", i); 
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if(i == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
 	}
 }
