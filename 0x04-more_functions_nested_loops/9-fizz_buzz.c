a30547bd5d2fb7944e64474cd968389f15adbf6b
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-23 04:51:42 -0800

    More_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: function to print fizz buzz()
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("%c%c%c%c ", 'F', 'i', 'z', 'z');
+		else if (i % 5 == 0)
+			printf("%c%c%c%c ", 'B', 'u', 'z', 'z');
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("%c%c%c%c%c%c%c%c ", 'F', 'i', 'z', 'z', 'B', 'u', 'z', 'z');
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
