f06106f5f49326f2ab8a48ca6859659b4c2a4b83
Diana-nyamai <dnyamai.dn@gmail.com>
2023-02-23 05:02:22 -0800

    More_functions_nested_loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("%c%c%c%c ", 'F', 'i', 'z', 'z');
 		else if (i % 5 == 0)
 			printf("%c%c%c%c ", 'B', 'u', 'z', 'z');
-		else if (i % 3 == 0 && i % 5 == 0)
-			printf("%c%c%c%c%c%c%c%c ", 'F', 'i', 'z', 'z', 'B', 'u', 'z', 'z');
 		else
 			printf("%d ", i);
 	}
