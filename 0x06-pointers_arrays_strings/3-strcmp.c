dcf73af340455c52e0ee113223df676405db8b41
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-02 04:48:43 -0800

    Update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,17 +8,20 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, j = 0, res;
+	int i = 0, res;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			res = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			res = s1[i] - s2[i];
+		}
 		i++;
-	while (s2[j] != '\0')
-		j++;
-	if (i > j)
-		res = 15;
-	else if (i < j)
-		res = -15;
-	else
-		res = 0;
+	}
 	return (res);
 }
