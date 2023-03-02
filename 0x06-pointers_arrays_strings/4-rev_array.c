c0e71496ba1ed974e3e91837724d9aef10b0e104
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-02 05:58:12 -0800

    Update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int start = 0, end = n - 1; temp;
+	int start = 0, end = n - 1, temp;
 
 	while (start < end)
 	{
 		temp = a[start];
 		a[start] = a[end];
-		a[end] = a[start];
+		a[end] = temp;
 		start++;
 		end--;
 	}
