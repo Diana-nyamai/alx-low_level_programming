171effd9a4e6b6e98d58eb28ea94070de689efcf
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-10 05:59:51 -0800

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+* @argc: input
+* @argv: input
+* Description: function
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 1, mul = 1;
+
+	if (argc > 1)
+	{
+		for (; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+		printf("Error\n");
+
+	return (0);
+}
