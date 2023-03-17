ba1fc50e8e78e865bd705dbe4eb27556f0166bdd
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-17 05:35:44 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,8 +14,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	char *newptr, *oldptr;
 
-	if (new_size < old_size)
-		old_size = new_size;
 	if (new_size == old_size)
 		return (ptr);
 	if (ptr == NULL)
