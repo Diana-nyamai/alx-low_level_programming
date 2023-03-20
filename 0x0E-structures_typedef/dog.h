e2c26c8aa1817ed41ab1b2890cdb5f4d129a8f6b
Diana-nyamai <dnyamai.dn@gmail.com>
2023-03-20 08:22:04 -0700

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+  * struct dog - struct
+  * @name: first
+  * @age: second
+  * @owner: third
+  * Description: Struct
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
