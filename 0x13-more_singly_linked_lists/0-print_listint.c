7551595f8c8199f841e7a85743c31fcf92fa5358
Diana-nyamai <dnyamai.dn@gmail.com>
2023-04-04 01:09:37 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,7 +8,7 @@
 
 size_t print_listint(const listint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 	const listint_t *ptr;
 
 	if (h == NULL)
@@ -25,7 +25,6 @@ size_t print_listint(const listint_t *h)
 		count++;
 		ptr = ptr->next;
 	}
-	printf("%d\n", ptr->n);
 
 	return (count);
 }
