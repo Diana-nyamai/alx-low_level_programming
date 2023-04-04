264e4da0391880ebcc842b878aac4f4cd4166500
Diana-nyamai <dnyamai.dn@gmail.com>
2023-04-04 02:56:44 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+* add_nodeint_end - entry point
+* @head: input
+* @n: input
+* Return: node at the end
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr, *temp;
+
+	temp = malloc(sizeof(listint_t));
+
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	temp->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+
+	ptr = *head;
+
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = temp;
+	return (temp);
+}
