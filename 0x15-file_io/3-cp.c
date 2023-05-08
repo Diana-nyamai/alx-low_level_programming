2e036e5b373d49b07579e82f5410b1d7eee45725
Diana-nyamai <dnyamai.dn@gmail.com>
2023-05-08 16:59:58 -0700

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,8 +3,9 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include "main.h"
 
-void close_file(int from, int to);
+#define READF  "Error: Can't read from file %s\n"
 /**
   * main - entry point
   * @argc: input
@@ -15,67 +16,35 @@ void close_file(int from, int to);
 int main(int argc, char *argv[])
 {
 	char mem[1024];
-	int from, to;
+	int from, to, closef;
 	ssize_t readf, writef;
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	from = open(argv[1], O_RDONLY);
 	if (from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, READF, argv[1]), exit(98);
 	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	while (1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		close(from);
-		exit(99);
-	}
-	while ((readf = read(from, mem, 1024)) > 0)
-	{
-		writef = write(to, mem, readf);
-		if (writef != readf)
+		readf = read(from, mem, 1024);
+		if (readf == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]), exit(98);
+		if (readf > 0)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			close(from);
-			close(to);
-			exit(99);
-		}
+			writef = write(to, mem, readf);
+			if (writef == -1)
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		} else
+			break;
 	}
-	if (readf == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(from);
-		close(to);
-		exit(98);
-	}
-	close_file(from, to);
+	closef = close(from);
+	if (closef == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from), exit(100);
+	closef = close(to);
+	if (closef == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to), exit(100);
 	return (0);
-}
-
-/**
-  * close_file - entry point
-  * @from: input
-  * @to: input
-  * Return: nothing
-  */
-
-void close_file(int from, int to)
-{
-	if (close(from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", from);
-		exit(100);
-	}
-	if (close(to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", to);
-		exit(100);
-	}
-
 }
